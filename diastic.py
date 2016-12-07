8427b0659658028cf3eac3022540932beecb119b
Frank Giraffe <fgiraffe@gmail.com>
2016-12-06 16:38:45 -0800

    Significant rework and clean up/restructing of code Fixed bug that used empty words :

--- diastic.py
+++ diastic.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# http://intercapillaryspace.blogspot.com/\
+# 2012/03/mac-lows-diastic-process-in-gale.html
+
+
 import random
 
 # table of chars to strip and what to replace them with
@@ -24,7 +28,18 @@ BAD_CHARS_DICT = {"\n": " ",
                   ",": " "}
 
 
+def pick_random_word(word_list):
+    indx = random.randint(0, len(word_list) - 1)
+    return word_list[indx]
+
+
 def clean_source_text(raw_chars, bad_chars_dict):
+    """ remove any unwanetd characters from given text
+    raw_chars -- characters to be read and cleaned
+    bad_chars_dict -- dict of characters that are NOT allowed in raw_chars
+
+    returns character array of "clean" text
+    """
     cleaned_chars = raw_chars
 
     for (bad_char, new_char) in bad_chars_dict.items():
@@ -33,80 +48,71 @@ def clean_source_text(raw_chars, bad_chars_dict):
     return cleaned_chars
 
 
+def gather_source_words(file_name):
+    clean_words = []
+
+    # read in everything, warts and all
+    txt = open("../idle_noise_11_30.txt")
+    raw_contents = txt.read()
+    txt.close()
+
+    # remove unwanted chars
+    clean_text = clean_source_text(raw_contents, BAD_CHARS_DICT)
+
+    # remove duplicate words by adding them to a set
+    # also remove the 1 character words
+
+    word_list = clean_text.split(" ")
+    word_set = set()
+    for aWord in word_list:
+        if len(aWord) > 1:
+            word_set.add(aWord)
+
+    clean_words = list(word_set)
+
+    return clean_words
+
+
+def create_poem_diastic(poem_seed, poem_src_words):
+    poem_words = []
+
+    for char_idx in range(len(poem_seed)):
+        # find a word from the list that "matches"
+        target_char = seed_phrase[char_idx]
+        matched = False
+
+        for a_word in word_list:
+            if len(a_word) > char_idx and a_word[char_idx] == target_char:
+
+                # don't reuse the same word
+                if a_word in poem_words:
+                    print "REUSING"
+                    continue
+                else:
+                    poem_words.append(a_word)
+                    matched = True
+                break
+
+        if matched is False:
+            # failed to find a word
+            replacement_word = pick_random_word(word_list)
+            poem_words.append(replacement_word)
+
+    return poem_words
+
+
 # #######################################################
 # #######################################################
 
-random.seed()
+if __name__ == '__main__':
+    random.seed()
+    word_list = gather_source_words("../idle_noise_11_30.txt")
 
-# read in everything, warts and all
+    orig_seed_phrase = "idle noise in the morning"
+    # seed_phrase = "abcdefg"
+    seed_phrase = orig_seed_phrase.replace(" ", "")
 
-txt = open("../idle_noise_11_30.txt")
-raw_contents = txt.read()
-txt.close()
+    poem = create_poem_diastic(seed_phrase, word_list)
 
-# remove unwanted chars
-clean_text = clean_source_text(raw_contents, BAD_CHARS_DICT)
-
-# remove duplicate words
-word_list = clean_text.split(" ")
-word_set = set()
-for aWord in word_list:
-    word_set.add(aWord)
-
-# while len(word_set) > 0:
-#    a_word = word_set.pop()
-#    print a_word
-
-
-poem_words = []
-
-seed_phrase = "idle noise in the morning"
-# seed_phrase = "abcdefg"
-seed_words = seed_phrase.split(" ")
-seed_count = len(seed_words)
-seed_phrase = seed_phrase.replace(" ", "")
-
-
-def pick_random_word(word_list):
-    indx = random.randint(0, len(word_list) - 1)
-    return word_list[indx]
-
-
-for char_idx in range(len(seed_phrase)):
-    # find a word from the list that matches
-    target_char = seed_phrase[char_idx]
-    matched = False
-
-
-#    print "\nTarget Char:",
-#    print target_char,
-#    print char_idx
-
-    for a_word in word_list:
-        if len(a_word) > char_idx and a_word[char_idx] == target_char:
-
-            # don't reuse the same word
-            if a_word in poem_words:
-                print "REUSING"
-                continue
-            else:
-                # print "Adding ",
-                # print a_word,
-                # print " ind: ",
-                # print char_idx
-                poem_words.append(a_word)
-                matched = True
-            break
-
-    if matched is False:
-        # failed to find a word
-        replacement_word = pick_random_word(word_list)
-        poem_words.append(replacement_word)
-        print "FAILED on Target Char:",
-        print target_char,
-        print char_idx,
-        print "Using <%r> instead" % (replacement_word)
-
-
-for aw in poem_words:
-    print aw
+    for aw in poem:
+        print aw
